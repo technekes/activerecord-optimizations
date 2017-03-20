7109e0a4b74873496c63e05e30a87953439d0a7a
Sean Carey <sean.carey@technekes.com>
2017-03-20 18:23:11 -0400

    Create (better) seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,26 @@ logger.level     = Logger::INFO
 logger.formatter = Logger::Formatter.new
 Rails.logger = logger
 
+[User, Comment, Blog].each(&:delete_all)
+
+=begin
 10.times do |i|
   User.where(name: "Author #{i}").first_or_create
 end
+=end
 
 100.times do |i|
+  Blog.create(
+    title: "Blog #{i}",
+    user_id: rand(1...10)
+  )
+end
 
-  blog = Blog.where(title: "Blog #{i}").first_or_create do |blog|
-    blog.user_id = (1..10).to_a.sample
-  end
-
-  1000.times do |n|
-    Comment.where(blog_id: blog.id).first_or_create do |c|
-      c.user_id = (1..10).to_a.sample
-    end
-  end
+blog_ids = Blog.all.pluck(:id)
+10_000.times do |n|
+  Comment.create(
+    body: SecureRandom.hex,
+    user_id: rand(1...10),
+    blog_id: blog_ids.sample
+  )
 end
